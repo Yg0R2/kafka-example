0056e1ba5c382e2195e57268825d17a7b1a1a387
Tibor Kovacs <v-tikovacs@hotels.com>
2018-05-09 15:40:33 +0200

    Use Scheduled poll

--- src/main/java/yg0r2/tmp/kafka/MainKafkaApp.java
+++ src/main/java/yg0r2/tmp/kafka/MainKafkaApp.java
@@ -2,8 +2,10 @@ package yg0r2.tmp.kafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MainKafkaApp {
 
     public static void main(String[] args) {
--- src/main/java/yg0r2/tmp/kafka/SlowLaneListener.java
+++ src/main/java/yg0r2/tmp/kafka/SlowLaneListener.java
@@ -2,28 +2,44 @@ package yg0r2.tmp.kafka;
 
 import java.util.List;
 
+import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.google.common.collect.ImmutableList;
 
 @Component
 public class SlowLaneListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SlowLaneListener.class);
 
+    @Value("${kafka.slowLane.topic}")
+    private String slowLaneTopic;
+
     @Autowired
     private RequestProcessor requestProcessor;
+    @Autowired
+    private Consumer<String, String> slowLaneConsumer;
 
-    @KafkaListener(id = "slowLaneListener", containerFactory = "kafkaSlowLaneContainerFactory", topics = "${kafka.slowLane.topic}")
-    public void receive(List<ConsumerRecord<String, String>> records, Acknowledgment acknowledgment) {
+    //@Scheduled(fixedDelay=5000)
+    public void poll() {
+        ConsumerRecords<String, String> records = slowLaneConsumer.poll(1000);
+
+        List<ConsumerRecord<String, String>> result = new ImmutableList.Builder<ConsumerRecord<String, String>>()
+            .addAll(records.records(slowLaneTopic))
+            .build();
+
+        receive(result);
+    }
+
+    private void receive(List<ConsumerRecord<String, String>> records) {
         records.stream().forEach(this::processRecord);
-
-        LOGGER.info("Acknowledgment");
-        acknowledgment.acknowledge();
     }
 
     private void processRecord(ConsumerRecord<String, String> record) {
